0c4bee22d3f02b29a3ea32165c2ade734e66fa11
fernando <64593873+carabedo@users.noreply.github.com>
2021-08-25 13:00:04 -0300

    Update create_config.sh

--- create_config.sh
+++ create_config.sh
@@ -1,7 +1,5 @@
-mkdir -p ~/.streamlit
-
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+echo "[server]"  > ~/.streamlit/config.toml 
+echo "headless = true"  >> ~/.streamlit/config.toml
+echo "port = $PORT"  >> ~/.streamlit/config.toml
+echo "enableCORS = false"  >> ~/.streamlit/config.toml
